5cbaff143af06c0e53ada0ea62d5b50a1838113a
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2022-11-02 20:07:44 +0600

    First

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+import { IonButton, IonToggle } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+   components: { HelloWorld, IonButton, IonToggle }
+});
+</script>
+
+<template>
+   <main>
+      <ion-button>Default</ion-button>
+      <ion-toggle color="medium"></ion-toggle>
+      <hr>
+      <HelloWorld msg="You did it!" />
+   </main>
+</template>
