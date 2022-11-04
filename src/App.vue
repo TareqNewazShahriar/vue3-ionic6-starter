f9cfd6b9a9f7c236c220f4b5a9653aa63a0d5880
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2022-11-04 19:06:53 +0600

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,12 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
+   <header>
       <nav>
-        <RouterLink to="/">Home</RouterLink> |
-        <RouterLink to="/about">About</RouterLink>
+         <RouterLink to="/">Home</RouterLink> |
+         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
-  </header>
+   </header>
 
-  <RouterView />
+   <RouterView />
 </template>
